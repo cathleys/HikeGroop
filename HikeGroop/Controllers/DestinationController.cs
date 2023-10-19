f39ce2349072939add99ec3623d282cc5ddb3e02
Cath Leyson <108257596+cathleys@users.noreply.github.com>
2023-10-19 16:28:00 +0800

    create-methods(#5)
    Adds create controllers,views, viewmodels and implements cloudinary photo uploading.

--- HikeGroop/Controllers/DestinationController.cs
+++ HikeGroop/Controllers/DestinationController.cs
@@ -1,4 +1,6 @@
 ﻿using HikeGroop.Interfaces;
+using HikeGroop.Models;
+using HikeGroop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,10 +9,13 @@ namespace HikeGroop.Controllers
     public class DestinationController : Controller
     {
         private readonly IDestinationRepository _destinationRepository;
+        private readonly IPhotoService _photoService;
 
-        public DestinationController(IDestinationRepository destinationRepository)
+        public DestinationController(IDestinationRepository destinationRepository,
+            IPhotoService photoService)
         {
             _destinationRepository = destinationRepository;
+            _photoService = photoService;
         }
 
         public async Task<IActionResult> Index()
@@ -23,6 +28,47 @@ namespace HikeGroop.Controllers
         {
             var dest = await _destinationRepository.GetDestinationByIdAsync(id);
             return View(dest);
+        }
+
+        public  IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateDestinationViewModel destinationVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var imageResult = await _photoService.AddPhotoAsync(destinationVM.Image);
+
+                var newDestination = new Destination
+                {
+                    Title = destinationVM.Title,
+                    Description = destinationVM.Description,
+                    Image = imageResult.SecureUrl.AbsoluteUri,
+                    Itinerary = new Itinerary
+                    {
+                        MeetUp = destinationVM.Itinerary.MeetUp,
+                        JumpOffPoint = destinationVM.Itinerary.JumpOffPoint,
+                        Location = destinationVM.Itinerary.Location,
+                        Elevation = destinationVM.Itinerary.Elevation,
+                        DaysRequired = destinationVM.Itinerary.DaysRequired,
+
+                    }
+
+                };
+
+                await _destinationRepository.Add(newDestination);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+
+                ModelState.AddModelError("", "Photo upload failed");
+            }
+
+            return View(destinationVM);
         }
     }
 }
---
+++ HikeGroop/ViewModels/CreateGroupViewModel.cs
@@ -0,0 +1,16 @@
+﻿using HikeGroop.Models;
+
+namespace HikeGroop.ViewModels
+{
+    public class CreateGroupViewModel
+    {
+        public int Id { get; set; }
+       
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IFormFile Image { get; set; }
+     
+      
+        public Address? Address { get; set; }
+    }
+}
