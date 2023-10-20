aff37b653e5072ce13aaf088dee94a217d869f85
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-10-20 16:52:25 +0800

    Implements Identity

--- HikeGroop/Data/Seed.cs
+++ HikeGroop/Data/Seed.cs
@@ -1,5 +1,6 @@
 ﻿using HikeGroop.Data.Enums;
 using HikeGroop.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace HikeGroop.Data;
 
@@ -121,62 +122,58 @@ public class Seed
         }
     }
 
-    // public static async Task SeedUsersAndRolesAsync(IApplicationBuilder applicationBuilder)
-    // {
-    //     using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
-    //     {
-    //         //Roles
-    //         var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    public static async Task SeedUsersAndRolesAsync(IApplicationBuilder applicationBuilder)
+    {
+        using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+        {
+            //Roles
+            var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
-    //         if (!await roleManager.RoleExistsAsync(UserRoles.Admin))
-    //             await roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
-    //         if (!await roleManager.RoleExistsAsync(UserRoles.User))
-    //             await roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+            if (!await roleManager.RoleExistsAsync(UserRoles.Admin))
+                await roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
+            if (!await roleManager.RoleExistsAsync(UserRoles.Member))
+                await roleManager.CreateAsync(new IdentityRole(UserRoles.Member));
 
-    //         //Users
-    //         var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
-    //         string adminUserEmail = "cathdeveloper@gmail.com";
+            //Users
+            var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+            string adminUserEmail = "cathdev@gmail.com";
 
-    //         var adminUser = await userManager.FindByEmailAsync(adminUserEmail);
-    //         if (adminUser == null)
-    //         {
-    //             var newAdminUser = new AppUser()
-    //             {
-    //                 UserName = "cathdev",
-    //                 Email = adminUserEmail,
-    //                 EmailConfirmed = true,
-    //                 Address = new Address()
-    //                 {
-    //                     Street = "123 Main St",
-    //                     City = "Charlotte",
-    //                     State = "NC"
-    //                 }
-    //             };
-    //             await userManager.CreateAsync(newAdminUser, "Coding@1234?");
-    //             await userManager.AddToRoleAsync(newAdminUser, UserRoles.Admin);
-    //         }
+            var adminUser = await userManager.FindByEmailAsync(adminUserEmail);
+            if (adminUser == null)
+            {
+                var newAdminUser = new AppUser
+                {
+                    UserName = "cathdev",
+                    Email = adminUserEmail,
+                    EmailConfirmed = true,
+                    Address = new Address
+                    {
+                        City = "Pasig City",
+                    }
+                };
+                await userManager.CreateAsync(newAdminUser, "Pa$$w0rd");
+                await userManager.AddToRoleAsync(newAdminUser, UserRoles.Admin);
+            }
 
-    //         string appUserEmail = "user@etickets.com";
+            string appUserEmail = "user@etickets.com";
 
-    //         var appUser = await userManager.FindByEmailAsync(appUserEmail);
-    //         if (appUser == null)
-    //         {
-    //             var newAppUser = new AppUser()
-    //             {
-    //                 UserName = "app-user",
-    //                 Email = appUserEmail,
-    //                 EmailConfirmed = true,
-    //                 Address = new Address()
-    //                 {
-    //                     Street = "123 Main St",
-    //                     City = "Charlotte",
-    //                     State = "NC"
-    //                 }
-    //             };
-    //             await userManager.CreateAsync(newAppUser, "Coding@1234?");
-    //             await userManager.AddToRoleAsync(newAppUser, UserRoles.User);
-    //         }
-    //     }
-    // }
+            var appUser = await userManager.FindByEmailAsync(appUserEmail);
+            if (appUser == null)
+            {
+                var newAppUser = new AppUser
+                {
+                    UserName = "member",
+                    Email = appUserEmail,
+                    EmailConfirmed = true,
+                    Address = new Address
+                    {
+                        City = "Pasig City",
+                    }
+                };
+                await userManager.CreateAsync(newAppUser, "Pa$$w0rd");
+                await userManager.AddToRoleAsync(newAppUser, UserRoles.Member);
+            }
+        }
+    }
 
 }
