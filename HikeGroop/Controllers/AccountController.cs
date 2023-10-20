a866935434f08abe1cdb04502b817b8d130ec57e
Cath Leyson <108257596+cathleys@users.noreply.github.com>
2023-10-20 19:34:37 +0800

    login-register-page (#8)

---
+++ HikeGroop/Controllers/AccountController.cs
@@ -0,0 +1,90 @@
+﻿using HikeGroop.Data;
+using HikeGroop.Models;
+using HikeGroop.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HikeGroop.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+           _userManager = userManager;
+        }
+
+        public IActionResult Login()
+        {
+            var login = new LoginViewModel(); 
+            return View(login);//preserving data when user loads page.
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid) return View(loginViewModel);
+
+            var user = await _userManager.FindByEmailAsync(loginViewModel.EmailAddress);
+
+            if (user == null)
+            {
+                TempData["Error"] = "Invalid username, Please try again"; 
+                return View(loginViewModel);
+            }
+
+            var isPasswordCorrect = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
+           
+            if(!isPasswordCorrect)
+            {
+                TempData["Error"] = "Invalid password, Please try again";
+                return View(loginViewModel);
+            }
+
+            return RedirectToAction("Index", "Home");
+
+        }
+        public async Task<IActionResult> Register()
+        {
+            var register = new RegisterViewModel();
+            return View(register);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid) return View(registerViewModel);
+
+           if(await UserExists(registerViewModel.Username)){
+
+                TempData["Error"] = "Username is taken, please try again";
+                return View(registerViewModel);
+
+            }
+
+            var newUser = new AppUser
+            {
+                UserName = registerViewModel.Username,
+                Email = registerViewModel.EmailAddress
+            };
+
+            var newUserResult = await _userManager.CreateAsync(newUser, registerViewModel.Password);
+           
+            if (newUserResult.Succeeded)
+            {
+            await _userManager.AddToRoleAsync(newUser, UserRoles.Member);
+            return View("Login");
+            }
+            return View(registerViewModel);
+
+        }
+
+
+        private async Task<bool>UserExists(string username)
+        {
+            return await _userManager.Users.AnyAsync(u => u.UserName == username);
+        }
+    }
+}
---
+++ HikeGroop/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace HikeGroop.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Username { get; set; } 
+        [Required(ErrorMessage="Email address is required")]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+       
+        public string Password { get; set; }
+        [Required]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage =" Password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
