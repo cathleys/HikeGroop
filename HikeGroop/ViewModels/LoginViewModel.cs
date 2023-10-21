c4e2e624ec2009f33d4907558c7d97cb3fe3b6b3
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-10-21 11:26:34 +0800

    adds logout function and user roles

--- HikeGroop/ViewModels/LoginViewModel.cs
+++ HikeGroop/ViewModels/LoginViewModel.cs
@@ -4,16 +4,13 @@ namespace HikeGroop.ViewModels
 {
     public class LoginViewModel
     {
-        [Required(ErrorMessage ="Username is required")]
-        public string Username { get; set; }
-
         [Display(Name = "Email Address")]
-        [Required(ErrorMessage ="Email address is required")]
+        [Required(ErrorMessage = "Email address is required")]
         public string EmailAddress { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
-        
+
     }
 }
