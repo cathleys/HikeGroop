38d2f73d0ebb307f649cc164977d7784c6c9a777
Cath Leyson <108257596+cathleys@users.noreply.github.com>
2023-10-22 13:57:58 +0800

    geo-location (#12)

---
+++ HikeGroop/ViewModels/GroupDestinationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using HikeGroop.Models;
+
+namespace HikeGroop.ViewModels;
+
+public class HomeViewModel
+{
+    public IEnumerable<Group>? Groups { get; set; }
+
+    public string? City { get; set; }
+    public string? Region { get; set; }
+
+}
