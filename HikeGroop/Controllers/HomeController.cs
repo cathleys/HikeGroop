99946d11c8fca90f85b3776f21d7af712a62b7d1
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-10-22 13:52:25 +0800

    Adds Ipinfo to controller and home view

--- HikeGroop/Controllers/HomeController.cs
+++ HikeGroop/Controllers/HomeController.cs
@@ -2,6 +2,12 @@
 using Microsoft.AspNetCore.Mvc;
 using HikeGroop.Models;
 using HikeGroop.Interfaces;
+using HikeGroop.Helpers;
+using HikeGroop.ViewModels;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Globalization;
+using Newtonsoft.Json;
 
 namespace HikeGroop.Controllers;
 
@@ -9,19 +15,60 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
     private readonly IGroupRepository _groupRepository;
+    private readonly IDestinationRepository _destinationRepository;
+    private readonly IPInfoSettings _ipInfoOptions;
 
-    public HomeController(ILogger<HomeController> logger, IGroupRepository groupRepository)
+    public HomeController(
+    ILogger<HomeController> logger,
+    IGroupRepository groupRepository,
+    IDestinationRepository destinationRepository,
+    IOptions<IPInfoSettings> ipInfoOptions
+    )
     {
         _logger = logger;
-       _groupRepository = groupRepository;
+        _groupRepository = groupRepository;
+        _destinationRepository = destinationRepository;
+        _ipInfoOptions = ipInfoOptions.Value;
     }
 
     public async Task<IActionResult> Index()
     {
-        var groups = await _groupRepository.GetGroups();
-        return View(groups);
-    }  
-    
+        _ = new IPInfo();
+        var homeViewModel = new HomeViewModel();
+
+        try
+        {
+            var url = $"https://ipinfo.io?token={_ipInfoOptions.Token}";
+            var info = new WebClient().DownloadString(url);
+            IPInfo? ipInfo = JsonConvert.DeserializeObject<IPInfo>(info);
+
+            RegionInfo regionInfo = new RegionInfo(ipInfo.Country);
+
+            ipInfo.Country = regionInfo.EnglishName;
+
+            homeViewModel.City = ipInfo.City;
+            homeViewModel.Region = ipInfo.Region;
+
+
+            if (homeViewModel.City != null)
+            {
+                homeViewModel.Groups = await _groupRepository
+                .GetGroupsByCity(homeViewModel.City);
+            }
+            else
+            {
+                homeViewModel.Groups = null;
+            }
+            return View(homeViewModel);
+        }
+        catch (System.Exception)
+        {
+            homeViewModel.Groups = null;
+        }
+        return View(homeViewModel);
+    }
+
+
 
     public IActionResult Privacy()
     {
@@ -33,4 +80,5 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
 }
---
+++ HikeGroop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using HikeGroop.Models;
+
+namespace HikeGroop;
+
+public class GroupDestinationViewModel
+{
+    public IEnumerable<Group> Groups { get; set; }
+    public IEnumerable<Destination> Destinations { get; set; }
+
+}
