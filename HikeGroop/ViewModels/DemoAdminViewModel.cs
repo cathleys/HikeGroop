5be19fa18e02ca10f47b2ac6af3a428a60288622
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-10-27 12:59:41 +0800

    Adds demo users

---
+++ HikeGroop/ViewModels/DemoAdminViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HikeGroop.ViewModels;
+
+public class DemoAdminViewModel
+{
+    public string Username { get; set; } = "admin";
+    public string EmailAddress { get; set; } = "admin@gmail.com";
+    public string Password { get; set; } = "Pa$$w0rd";
+}
