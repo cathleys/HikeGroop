446b33ac8255c7205f6c9909ce38875e69ff3993
Cath Leyson <108257596+cathleys@users.noreply.github.com>
2023-11-12 17:07:40 +0800

    fix-dest-interest-btn (#42)

--- HikeGroop/wwwroot/assets/js/interested.js
+++ HikeGroop/wwwroot/assets/js/interested.js
@@ -1,5 +1,15 @@
-const btn = document.querySelector(".classic-btn");
+const destBtns = document.querySelectorAll(".classic-btn");
+const grpBtns = document.querySelectorAll(".main-btn2");
 
-btn.addEventListener("click", () => {
-  btn.innerHTML = `<a>Going<a>`;
+destBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.innerHTML = "<a>Going<a>";
+  });
+});
+
+grpBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.innerHTML = `<a><i
+    class="icofont-check-circled"></i> Joined<a>`;
+  });
 });
